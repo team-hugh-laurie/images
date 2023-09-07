67985466a809cac7b0be72e34886a0f1433098db
Vladimir Klepov <vklepov@ozon.ru>
2023-09-07 14:53:43 +0300

    remove helper files from gh pages

--- generateManifest.js
+++ generateManifest.js
@@ -3,10 +3,10 @@ import path from 'path';
 
 const root = '/images/';
 const manifest = {};
-for (const file of fs.readdirSync('.')) {
+for (const file of fs.readdirSync('./images')) {
   const name = path.basename(file);
   if (name.endsWith('.png')) {
     manifest[name.replace('.png', '')] = `${root}${name}`;
   }
 }
-fs.writeFileSync('manifest.json', JSON.stringify(manifest));
+fs.writeFileSync('./images/manifest.json', JSON.stringify(manifest));
